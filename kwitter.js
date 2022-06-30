7fa5d8c1e979a21a291ddf16559f96df7160ae1e
dakshesh252 <101445356+dakshesh252@users.noreply.github.com>
2022-06-30 00:14:59 -0700

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,11 @@
+
+
+ function addUser() {
+
+    user_name = document.getElementById("user-name").value;
+
+    localStorage.setItem("user-name : " , user_name );
+
+    window.location="kwitter_room.html";
+
+ }
